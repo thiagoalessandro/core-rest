092ce55ec4036abc0cdddf7385673e204e2dee94
thiago <thiago.alessandro.farias@gmail.com>
2020-01-20 23:51:51 -0300

    Arquitetura de microserviço

---
+++ src/main/java/br/com/intelector/microservice/corerest/CoreRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.intelector.microservice.corerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/intelector/microservice/corerest/CoreRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intelector.microservice.corerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
